c59fcd9736573212c47e9f20b197aa7a4ad5d580
Camille HUOT <camille@huot.name>
2020-10-15 18:45:37 +0200

    Add created_date

--- src/main/java/registrar/Person.java
+++ src/main/java/registrar/Person.java
@@ -1,8 +1,11 @@
 package registrar;
 
+import java.time.LocalDateTime;
+
 public class Person {
     private String email;
     private String name;
+    private LocalDateTime created_date = LocalDateTime.now();
 
     public Person(String email, String name) {
         this.email = email;
